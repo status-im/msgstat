53fbfc15d1c10a20bce32a9342380aec20a6a787
Alexander Ewetumo <trinoxf@gmail.com>
2017-11-02 16:06:10 +0100

    Add makefile and travis.yml for CI

--- stats/stats_test.go
+++ stats/stats_test.go
@@ -15,28 +15,28 @@ import (
 func TestAggregationReadToJSON(t *testing.T) {
 	reader, err := os.Open("../fixtures/status.log")
 	if err != nil {
-		t.Fatalf("Should have succesfully opened log file")
+		t.Fatalf("Should have successfully opened log file")
 	}
-	t.Logf("Should have succesfully opened log file")
+	t.Logf("Should have successfully opened log file")
 
 	var buf bytes.Buffer
 	if err := stats.ReadAggregates(reader, &writeCloser{&buf}, "json"); err != nil {
-		t.Fatalf("Should have succesfully processed log data")
+		t.Fatalf("Should have successfully processed log data")
 	}
-	t.Logf("Should have succesfully processed log data")
+	t.Logf("Should have successfully processed log data")
 
 	expected, err := ioutil.ReadFile("../fixtures/expected.json")
 	if err != nil {
-		t.Fatalf("Should have succesfully opened expected output file")
+		t.Fatalf("Should have successfully opened expected output file")
 	}
-	t.Logf("Should have succesfully opened expected output file")
+	t.Logf("Should have successfully opened expected output file")
 
 	if !bytes.Equal(expected, buf.Bytes()) {
 		t.Logf("Expected: %+q\n", expected)
 		t.Logf("Received: %+q\n", buf.Bytes())
-		t.Fatalf("Should have succesfully matched aggregated data with expected data")
+		t.Fatalf("Should have successfully matched aggregated data with expected data")
 	}
-	t.Logf("Should have succesfully matched aggregated data with expected data")
+	t.Logf("Should have successfully matched aggregated data with expected data")
 }
 
 // TestAggregationReadToTOML validates the generation of aggregation data into
@@ -44,28 +44,28 @@ func TestAggregationReadToJSON(t *testing.T) {
 func TestAggregationReadToTOML(t *testing.T) {
 	reader, err := os.Open("../fixtures/status.log")
 	if err != nil {
-		t.Fatalf("Should have succesfully opened log file")
+		t.Fatalf("Should have successfully opened log file")
 	}
-	t.Logf("Should have succesfully opened log file")
+	t.Logf("Should have successfully opened log file")
 
 	var buf bytes.Buffer
 	if err := stats.ReadAggregates(reader, &writeCloser{&buf}, "toml"); err != nil {
-		t.Fatalf("Should have succesfully processed log data")
+		t.Fatalf("Should have successfully processed log data")
 	}
-	t.Logf("Should have succesfully processed log data")
+	t.Logf("Should have successfully processed log data")
 
 	expected, err := ioutil.ReadFile("../fixtures/expected.toml")
 	if err != nil {
-		t.Fatalf("Should have succesfully opened expected output file")
+		t.Fatalf("Should have successfully opened expected output file")
 	}
-	t.Logf("Should have succesfully opened expected output file")
+	t.Logf("Should have successfully opened expected output file")
 
 	if !bytes.Equal(expected, buf.Bytes()) {
 		t.Logf("Expected: %+q\n", expected)
 		t.Logf("Received: %+q\n", buf.Bytes())
-		t.Fatalf("Should have succesfully matched aggregated data with expected data")
+		t.Fatalf("Should have successfully matched aggregated data with expected data")
 	}
-	t.Logf("Should have succesfully matched aggregated data with expected data")
+	t.Logf("Should have successfully matched aggregated data with expected data")
 }
 
 // TestAggregationReadToYAML validates the generation of aggregation data into
@@ -73,28 +73,28 @@ func TestAggregationReadToTOML(t *testing.T) {
 func TestAggregationReadToYAML(t *testing.T) {
 	reader, err := os.Open("../fixtures/status.log")
 	if err != nil {
-		t.Fatalf("Should have succesfully opened log file")
+		t.Fatalf("Should have successfully opened log file")
 	}
-	t.Logf("Should have succesfully opened log file")
+	t.Logf("Should have successfully opened log file")
 
 	var buf bytes.Buffer
 	if err := stats.ReadAggregates(reader, &writeCloser{&buf}, "yaml"); err != nil {
-		t.Fatalf("Should have succesfully processed log data")
+		t.Fatalf("Should have successfully processed log data")
 	}
-	t.Logf("Should have succesfully processed log data")
+	t.Logf("Should have successfully processed log data")
 
 	expected, err := ioutil.ReadFile("../fixtures/expected.yaml")
 	if err != nil {
-		t.Fatalf("Should have succesfully opened expected output file")
+		t.Fatalf("Should have successfully opened expected output file")
 	}
-	t.Logf("Should have succesfully opened expected output file")
+	t.Logf("Should have successfully opened expected output file")
 
 	if !bytes.Equal(expected, buf.Bytes()) {
 		t.Logf("Expected: %+q\n", expected)
 		t.Logf("Received: %+q\n", buf.Bytes())
-		t.Fatalf("Should have succesfully matched aggregated data with expected data")
+		t.Fatalf("Should have successfully matched aggregated data with expected data")
 	}
-	t.Logf("Should have succesfully matched aggregated data with expected data")
+	t.Logf("Should have successfully matched aggregated data with expected data")
 }
 
 // writeCloser provides a decorator which adds a `Close` method to a composed io.Writer.
